0f9cafde09eda291be27d12ba8b1590aa0381099
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-07 12:41:58 -0500

    Add files via upload

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,6 +5,30 @@ public class ArraySequence implements IntegerSequence {
   /*Construct the sequence by copying values from the other array into the data array*/
   public ArraySequence(int[] other){
     this.data = other;
+    this.currentIndex = 0;
+  }
+
+  public void reset(){
+    this.data = [];
+    this.currentIndex = 0;
+  }
+
+  public int length(){
+    return (this.data.size());
+  }
+
+  public boolean hasNext(){
+    // return (this.current >= this.start && this.current <= this.end);
+  }
+
+  //@throws NoSuchElementException
+  public int next(){
+    // if (!this.hasNext()) {
+    //   throw new NoSuchElementException();
+    // } else {
+    //   this.current++;
+    //   return this.current;
+    // }
   }
 
 
