4deaf5f7f9628dd6f17bbca230527d25601b0b24
carolchen1120 <72897491+carolchen1120@users.noreply.github.com>
2020-12-07 12:51:33 -0500

    Add files via upload

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,4 +1,5 @@
 import java.util.NoSuchElementException;
+import java.lang.reflect.Array;
 
 public class ArraySequence implements IntegerSequence{
   int currentIndex;
@@ -20,19 +21,17 @@ public class ArraySequence implements IntegerSequence{
   }
 
   public boolean hasNext(){
-    // return (this.current >= this.start && this.current <= this.end);
-    return true;
+    return (this.currentIndex >= 0 && this.currentIndex < this.length());
   }
 
   //@throws NoSuchElementException
   public int next(){
-    // if (!this.hasNext()) {
-    //   throw new NoSuchElementException();
-    // } else {
-    //   this.current++;
-    //   return this.current;
-    // }
-    return 0;
+    if (!this.hasNext()) {
+      throw new NoSuchElementException();
+    } else {
+      this.currentIndex++;
+      return (int)Array.get(this.data, this.currentIndex);
+    }
   }
 
 
